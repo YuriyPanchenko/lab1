9a92e4ba9c07dfc913288de7f883381e1306fed9
YuriyPanchenko <yuriy.panchenko.2000@gmail.com>
2020-02-29 20:54:36 +0200

    create necessary classes

---
+++ paragraph7/Train.js
@@ -0,0 +1,11 @@
+class Train {
+
+  constructor (options) {
+    this.id = options.id;
+    this.direction = options.direction;
+    this.vagons = options.vagons;
+  }
+
+}
+
+module.exports = Train;
